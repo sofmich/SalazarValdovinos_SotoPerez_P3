27ef5bd43613d2e10dac422a509096691d2e3d1e
Sofi <ie714715@iteso.mx>
2020-04-16 11:08:49 -0500

    First files

---
+++ LED_MATRIX.c
@@ -0,0 +1,9 @@
+/**
+	\file
+	\brief
+		This is the API for control of a LED MATRIX using Kinetis FRDMK64
+	\author Sofía Salazar, Omar Soto
+	\date	16/04/2020
+	\todo
+
+ */
---
+++ LED_MATRIX.h
@@ -0,0 +1,16 @@
+/**
+	\file
+	\brief
+		This is the API for control of a LED MATRIX using Kinetis FRDMK64
+	\author Sofía Salazar, Omar Soto
+	\date	16/04/2020
+	\todo
+
+ */
+
+#ifndef LED_MATRIX_H_
+#define LED_MATRIX_H_
+
+
+
+#endif /* LED_MATRIX_H_ */
