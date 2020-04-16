f9b2f78380d0529763f039800d843339dd7bf944
Sofi <ie714715@iteso.mx>
2020-04-16 14:23:33 -0500

    not tested

--- LED_MATRIX.c
+++ LED_MATRIX.c
@@ -7,3 +7,74 @@
 	\todo
 
  */
+
+#include "LED_MATRIX.h"
+#include "MK64F12.h"
+#include "I2C.h"
+
+/** For testing matrix, these are the address used for it*/
+uint8_t matrix_test[] = {0x81, 0x81, 0x70, 0x07, 0x60, 0x68, 0x50, 0x05, 0x40, 0x04, 0x30, 0x15, 0x2F, 0x02, 0x10, 0x01};
+void MATRIX_init(void)
+{
+	/** Start sending the third byte configuration*/
+	I2C_tx_rx_mode(TRANSMITTER);
+	I2C_start();
+
+	I2C_write_byte(MATRIX_WRITE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_write_byte(FIRST_INIT_VALUE);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_stop();
+	/** Start sending the second byte configuration*/
+	I2C_start();
+
+	I2C_write_byte(MATRIX_WRITE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_write_byte(SECOND_INIT_VALUE);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_stop();
+
+	/** Start sending the third byte configuration*/
+	I2C_start();
+
+	I2C_write_byte(MATRIX_WRITE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_write_byte(THIRD_INIT_VALUE);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_stop();
+
+}
+
+void MATRIX_test(void)
+{
+	I2C_start();
+	/**Start communicating with matrix */
+	I2C_write_byte(MATRIX_WRITE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+
+	I2C_write_byte(MATRIX_MEM_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+
+	for(uint8_t i = 0; i < 16; i ++  )
+	{
+		I2C_write_byte(matrix_test[i]);
+		I2C_wait();
+		I2C_get_ack();
+	}
+
+	I2C_stop();
+}
--- LED_MATRIX.h
+++ LED_MATRIX.h
@@ -1,7 +1,7 @@
 /**
 	\file
 	\brief
-		This is the API for control of a LED MATRIX using Kinetis FRDMK64
+		This is the API for control of a LED MATRIX HT16K33 using Kinetis FRDMK64
 	\author Sofía Salazar, Omar Soto
 	\date	16/04/2020
 	\todo
@@ -11,6 +11,18 @@
 #ifndef LED_MATRIX_H_
 #define LED_MATRIX_H_
 
+/** Address of the matrix to comunicate through I2C*/
+#define MATRIX_WRITE_ADDRESS	0x0E
+/** Mem address*/
+#define MATRIX_MEM_ADDRESS		0U
+/** Initialization constant 1*/
+#define FIRST_INIT_VALUE		0x21
+#define SECOND_INIT_VALUE		0x81
+#define THIRD_INIT_VALUE		0xEF
+
+
+void MATRIX_init(void);
+void MATRIX_test(void);
 
 
 #endif /* LED_MATRIX_H_ */
