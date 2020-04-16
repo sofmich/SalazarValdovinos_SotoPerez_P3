6e678480c35488feaf19441e453896d79a33db46
Sofi <ie714715@iteso.mx>
2020-04-16 11:06:06 -0500

    First files

---
+++ RAM.c
@@ -0,0 +1,9 @@
+/**
+	\file
+	\brief
+		This is the driver for write and read functions of a I2C RAM 24AA256
+	\author Sofía Salazar, Omar Soto
+	\date	16/04/2020
+	\todo
+
+ */
---
+++ RAM.h
@@ -0,0 +1,16 @@
+/**
+	\file
+	\brief
+		This is the driver for write and read functions of a I2C RAM 24AA256
+	\author Sofía Salazar, Omar Soto
+	\date	16/04/2020
+	\todo
+
+ */
+
+#ifndef RAM_H_
+#define RAM_H_
+
+
+
+#endif /* RAM_H_ */
