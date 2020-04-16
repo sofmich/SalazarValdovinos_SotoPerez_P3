84ab25ceb8ba59876fdf12768b16fbbaf0a53fbc
Sofi <ie714715@iteso.mx>
2020-04-16 16:27:49 -0500

    changes on i2c and system control

--- MCP7940.c
+++ MCP7940.c
@@ -5,7 +5,6 @@
 	\author Omar Soto, Sofia Salazar
 	\date	05/04/2020
 	\todo
-		To implement the UART_init function
  */
 
 #include "MCP7940.h"
@@ -25,184 +24,87 @@ void Init_MCP7940(i2c_channel_t channel,  i2c_baud_rate_t baudrate){
 	/** Initialization hour*/
 	uint8_t init_hour[] = {0,0,0,0,0,0};
 	/** Set the init hour*/
-	UpdateTime(init_hour);
+	UpdateTime(TIME, init_hour);
 	/** Generate a 1Hz Clock*/
 	RTCenable();
 }
 
 void RTCenable(void){
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
+	I2C_tx_rx_mode(I2C_0,TRANSMITTER);
+	I2C_start(I2C_0);
 
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,AD_WRITE);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_write_byte(CONTROL);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,CONTROL);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_write_byte(MFT);
-	I2C_wait();
-	I2C_get_ack();
-	I2C_stop();
+	I2C_write_byte(I2C_0,MFT);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
+	I2C_stop(I2C_0);
 }
 
-void SetSecond(uint8_t sec){
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
+void RTC_write_byte(uint8_t address, uint8_t data)
+{
+	I2C_tx_rx_mode(I2C_0,TRANSMITTER);
+	I2C_start(I2C_0);
 
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,AD_WRITE);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_write_byte(SEC);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,address);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_write_byte(ST | sec);
-	I2C_wait();
-	I2C_get_ack();
-	I2C_stop();
+	I2C_write_byte(I2C_0,data);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
+	I2C_stop(I2C_0);
+
 }
-
-void SetMinute(uint8_t min){
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
-
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(MIN);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(min);
-	I2C_wait();
-	I2C_get_ack();
-	I2C_stop();
-}
-
-void SetHour(uint8_t hour){
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
-
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(HOUR);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(hour);
-	I2C_wait();
-	I2C_get_ack();
-	I2C_stop();
-}
-
-uint8_t GetSecond(void){
+uint8_t RTC_read_byte(uint8_t address)
+{
 	uint8_t data;
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
+	I2C_tx_rx_mode(I2C_0,TRANSMITTER);
+	I2C_start(I2C_0);
 
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,AD_WRITE);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_write_byte(SEC);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_write_byte(I2C_0,address);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_repeted_start();
-	I2C_write_byte(AD_READ);
-	I2C_wait();
-	I2C_get_ack();
+	I2C_repeted_start(I2C_0);
+	I2C_write_byte(I2C_0,AD_READ);
+	I2C_wait(I2C_0);
+	I2C_get_ack(I2C_0);
 
-	I2C_tx_rx_mode(RECEIVER);
+	I2C_tx_rx_mode(I2C_0,RECEIVER);
 
-	I2C_nack();
-	data = I2C_read_byte();
-	I2C_wait();
-	I2C_nack();
+	I2C_nack(I2C_0);
+	data = I2C_read_byte(I2C_0);
+	I2C_wait(I2C_0);
+	I2C_nack(I2C_0);
 
-	I2C_stop();
-	data = I2C_read_byte();
+	I2C_stop(I2C_0);
+	data = I2C_read_byte(I2C_0);
 
 	return data;
 }
 
-uint8_t GetMinute(void){
-	uint8_t data;
-
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
-
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(MIN);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_repeted_start();
-	I2C_write_byte(AD_READ);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_tx_rx_mode(RECEIVER);
-
-	I2C_nack();
-	data = I2C_read_byte();
-	I2C_wait();
-	I2C_nack();
-
-	I2C_stop();
-	data = I2C_read_byte();
-
-	return data;
-}
-
-uint8_t GetHour(void){
-
-	uint8_t data;
-
-	I2C_tx_rx_mode(TRANSMITTER);
-	I2C_start();
-
-	I2C_write_byte(AD_WRITE);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_write_byte(HOUR);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_repeted_start();
-	I2C_write_byte(AD_READ);
-	I2C_wait();
-	I2C_get_ack();
-
-	I2C_tx_rx_mode(RECEIVER);
-
-	I2C_nack();
-	data = I2C_read_byte();
-	I2C_wait();
-	I2C_nack();
-
-	I2C_stop();
-	data = I2C_read_byte();
-	return data;
-}
 
 void UpdateDisplayTime(void)
 {
 	/** Structure to save all elements of time format*/
 	time_format_t actual_time;
 	/** Get hours and print them on UART*/
-	uint8_t temp_time = GetHour();
+	uint8_t temp_time = RTC_read_byte(HOUR);
 	delay(DELAY);
 	printf("==============TEMP TIME HRS %x \n", temp_time);
 	if(temp_time < 12)
@@ -220,14 +122,14 @@ void UpdateDisplayTime(void)
 	/** Get hours units*/
 	actual_time.units_hours = temp_time & UNITS_MASK;
 	/** Get minutes and print them on UART*/
-	temp_time = GetMinute();
+	temp_time = RTC_read_byte(MIN);
 	delay(DELAY);
 	/** Get minutes teens*/
 	actual_time.tens_minutes = (temp_time & TENS_MINS) >>  CENTS_SHIFTER;
 	/** Get minutes units*/
 	actual_time.units_minutes = temp_time & UNITS_MASK;
 	/** Get seconds and print them on UART*/
-	temp_time = GetSecond();
+	temp_time = RTC_read_byte(SEC);
 	delay(DELAY);
 	/** Get seconds teens*/
 	actual_time.tens_seconds = (temp_time & TENS_SEC) >> CENTS_SHIFTER;
@@ -241,7 +143,7 @@ void UpdateDisplayTime(void)
 			actual_time.am_pm);
 
 	/** VT100 command for positioning the cursor in x and y position*/
-	UART_put_string(UART_0,"\033[11;10H");
+	UART_put_string(UART_0,"\033[12;10H");
 	/** Print chars of display time HH:MM:SS am*/
 	uint8_t dots = ASCII_DOTS;
 	UART_put_char(UART_0, actual_time.tens_hours + ASCII_CONST);
@@ -264,26 +166,85 @@ void UpdateDisplayTime(void)
 
 
 }
-
-void UpdateTime(uint8_t time[])
+void UpdateDisplayDate(void)
 {
+	uint8_t date_format[3];
+	date_format_t actual_date = {FALSE};
+	/** Read all digits of date*/
+	date_format[0] = RTC_read_byte(YEARS_ADDRESS);
+	date_format[1] = RTC_read_byte(MONTHS_ADDRESS);
+	date_format[2] = RTC_read_byte(DAYS_ADDRESS);
+	printf("++++++++++ Actual date\n %x / %x / %x \n",
+			date_format[2], date_format[1], date_format[0]);
 
-	/**Time comes in format  DH UH  DM UM   DS US so let's get a byte to send*/
+	/** Adjust tens and units of each digit*/
+	actual_date.tens_years = (date_format[0] & YEARS_CENTS_MASK) >> CENTS_SHIFTER;
+	actual_date.units_years = (date_format[0] & UNITS_MASK);
+	/* Adjust tens and units of months*/
+	actual_date.tens_months = (date_format[1] & MONTHS_CENTS_MASK) >> CENTS_SHIFTER;
+	actual_date.units_months = (date_format[1] & UNITS_MASK);
+	/* Adjust tens and units of days*/
+	actual_date.tens_days = (date_format[2] & DAYS_CENTS_MASK) >>  CENTS_SHIFTER;
+	actual_date.units_days = (date_format[2] & UNITS_MASK);
+
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_put_string(UART_0,"\033[12;10H");
+	/** Print chars of display time HH:MM:SS am*/
+	UART_put_char(UART_0, actual_date.tens_days + ASCII_CONST);
+	UART_put_char(UART_0, actual_date.units_days + ASCII_CONST);
+	UART_put_char(UART_0, '/');
+	UART_put_char(UART_0, actual_date.tens_months + ASCII_CONST);
+	UART_put_char(UART_0, actual_date.units_months  + ASCII_CONST);
+	UART_put_char(UART_0, '/');
+	UART_put_char(UART_0, actual_date.tens_years + ASCII_CONST);
+	UART_put_char(UART_0, actual_date.units_years  + ASCII_CONST);
+}
+
+void UpdateTime(config_rtc_t date_or_time, uint8_t time[])
+{
+	/**Time or date comes in format  CENTS UNITS/CENTS UNITS/CENTS/UNITS  so let's get a byte to send*/
 	uint8_t sec = (time[SECONDS_TENS] << CENTS_SHIFTER) | (time[SECONDS_UNITS]);
 	uint8_t min = (time[MINUTES_TENS] << CENTS_SHIFTER) | (time[MINUTES_UNITS]);
 	uint8_t hor = (time[HOURS_TENS] << CENTS_SHIFTER) | (time[	HOURS_UNITS]);
-	/** Allows to set a new hour*/
-	SetHour(hor);
-	/** Give time to I2C to finish transfer*/
-	delay(DELAY);
-	/** Allows to set a new minutes value*/
-	SetMinute(min);
-	/** Give time to I2C to finish transfer*/
-	delay(DELAY);
-	/** Allows to set a new seconds value*/
-	SetSecond(sec);
-	/** Give time to I2C to finish transfer*/
-	delay(DELAY);
+	switch(date_or_time)
+	{
+	case(TIME):
+	{
+
+		/** Allows to set a new hour*/
+		RTC_write_byte(HOUR, hor);
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+		/** Allows to set a new minutes value*/
+		RTC_write_byte(MIN, min);
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+		/** Allows to set a new seconds value*/
+		RTC_write_byte(SEC, (ST |sec));
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+	}
+		break;
+	case(DATE):
+	{
+		/** Allows to set a new DAY*/
+		RTC_write_byte(DAYS_ADDRESS, hor);
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+		/** Allows to set a new month*/
+		RTC_write_byte(MONTHS_ADDRESS, min);
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+		/** Allows to set a new DAY*/
+		RTC_write_byte(YEARS_ADDRESS, sec);
+		/** Give time to I2C to finish transfer*/
+		delay(DELAY);
+
+	}
+	break;
+
+	}
+
 }
 
 
--- main.c
+++ main.c
@@ -28,43 +28,14 @@ int main(void)
 {
 	/**Init all system*/
 	uint8_t status = init_system();
-	system_set_status(status);
 	for(;;) {
 		if(TRUE == UART_get_interrupt_flag(UART_0))
 		{
 			UART_clear_interrupt_flag(UART_0);
 			/** Save the key the user has pressed*/
 			uint8_t data_from_user = UART_get_mailbox(UART_0);
-			/** Know which status is the actual one to make changes*/
-			status = get_status();
-			/** Always ESC key is pressed, return to main_menu*/
-			if(ESC_ASCII == data_from_user)
-			{
-				system_set_status(MAIN_MENU);
-				main_menu();
-			}
-			/** Once config option was activated, chars should go to update time*/
-			else if(CONFIG == status)
-			{
-				config_hour(data_from_user);
+			system_control(data_from_user);
 
-			}
-			if(MAIN_MENU == status)
-			{
-				/** When in main menu select wheter 1 or 2 was selected as an option*/
-				switch(data_from_user)
-				{
-				case('1'):
-				read_hour();
-				system_set_status(READ);
-				break;
-				case('2'):
-				display_config_hour();
-				system_set_status(CONFIG);
-				break;
-
-				}
-			}
 		}
 	}
 
